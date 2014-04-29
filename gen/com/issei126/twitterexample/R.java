dc8dd4b11b313fb9b924c83295a8fe51b1b401b5
issei126 <isseeei@gmail.com>
2014-04-29 14:22:34 +0900

    divide twitter strings from strings.xml

--- gen/com/issei126/twitterexample/R.java
+++ gen/com/issei126/twitterexample/R.java
@@ -23,9 +23,7 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int pen=0x7f020001;
-        public static final int play=0x7f020002;
-        public static final int rotate=0x7f020003;
-        public static final int stop=0x7f020004;
+        public static final int rotate=0x7f020002;
     }
     public static final class id {
         public static final int action_start_oauth=0x7f080002;
@@ -51,14 +49,14 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
-        public static final int menu_refresh=0x7f050007;
-        public static final int menu_start_stream=0x7f050009;
-        public static final int menu_stop_stream=0x7f05000a;
-        public static final int menu_tweet=0x7f050008;
-        public static final int twitter_callback_url=0x7f050006;
-        public static final int twitter_consumer_key=0x7f050003;
-        public static final int twitter_consumer_secret=0x7f050004;
-        public static final int twitter_oauth=0x7f050005;
+        public static final int menu_refresh=0x7f050005;
+        public static final int menu_start_stream=0x7f050007;
+        public static final int menu_stop_stream=0x7f050008;
+        public static final int menu_tweet=0x7f050006;
+        public static final int twitter_callback_url=0x7f050004;
+        public static final int twitter_consumer_key=0x7f050009;
+        public static final int twitter_consumer_secret=0x7f05000a;
+        public static final int twitter_oauth=0x7f050003;
     }
     public static final class style {
         /** 
